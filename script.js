fb5ab57e0233cf21ff64127c913f4b1054d1b0cc
Grunt395 <grantlew@att.net>
2024-08-21 12:37:14 -0700

    Added hex rand funcitonality

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-function getRandomNumber(params) {
+function getRandomNumber() {
     // Multiply by length of array (4)
     return Math.floor(Math.random() * colors.length);
 }
