6408333409b8d8493ba913f9723ec30a541e7211
Grunt395 <grantlew@att.net>
2024-08-21 12:17:48 -0700

    Added rand function

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
+function getRandomNumber(params) {
+    // Multiply by length of array (4)
+    return Math.floor(Math.random() * colors.length);
+}
+
 btn.addEventListener("click", function() {
     // Get random number between 0 and 3
-    const randomNumber = Math.floor(Math.random() * 4);
+    const randomNumber = getRandomNumber();
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
